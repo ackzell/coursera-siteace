7cace0a12d69dfead25a40b27322a8fb34f45336
ackzell <ackzell@gmail.com>
2015-12-27 18:07:02 -0600

    suggestion implemented

--- client/siteace.js
+++ client/siteace.js
@@ -4,20 +4,64 @@
 
 // helper function that returns all available websites
 Template.website_list.helpers({
-    // websites: function() {
-    //     return Websites.find({}, {
-    //         sort: {
-    //             upVotes: -1
-    //         }
-    //     });
-    // }
+    // https://github.com/matteodem/meteor-easy-search
     websites: function() {
         if (Session.get('searchQuery')) {
-            return  WebsitesIndex.search(Session.get('searchQuery')).fetch().sort({upVotes: -1});
+            return WebsitesIndex.search(Session.get('searchQuery')).fetch().sort({
+                upVotes: -1
+            });
         } else {
             return Websites.find({}, {
-                sort: {upVotes: -1}
+                sort: {
+                    upVotes: -1
+                }
             });
+        }
+    }
+});
+
+Template.suggested_sites.helpers({
+    terms: function() {
+        if (Session.get('lastTitle')) {
+            var terms = Session.get('lastTitle').trim().split(' '),
+                result = [];
+            terms.forEach(function (term) {
+                if (term.length > 2) {
+                    result.push({term: term});
+                }
+            });
+            return result;
+        }
+    },
+    websites: function() {
+        if (Session.get('lastTitle')) {
+            var terms = Session.get('lastTitle').trim().split(' '),
+                results = [];
+
+            // console.log('terms', terms);
+            terms.forEach(function(term) {
+                if (term.length > 2) {
+                    results = results.concat(WebsitesIndex.search(term).fetch());
+                }
+            });
+
+            results = results.sort(function(a, b) {
+                return a.upVotes > b.upVotes ? -1 : a.upVotes < b.upVotes ? 1 : 0;
+            });
+
+            // console.log('results', results);
+            // var results = WebsitesIndex.search(Session.get('lastTitle')).fetch().sort({upVotes: -1});
+            results = _.uniq(results, function(item, key, title) { 
+                return item.title;
+            });
+            return _.filter(results, function(site) {
+                // console.log('filtering on:', site);
+                // console.log('current title:', Session.get('lastTitle'));
+                 return site.originalTitle !== Session.get('lastTitle');
+            });
+
+        } else {
+            return [];
         }
     }
 });
@@ -26,8 +70,14 @@ Template.website_list.helpers({
 // template events 
 /////
 
+Template.suggested_sites.events({
+    'click .js-show-detail': function() {
+        $('#suggested_sites').modal('hide');
+    }
+});
+
 Template.searchBox.events({
-    'keyup #search-box': _.throttle(function (event) {
+    'keyup #search-box': _.throttle(function(event) {
         var text = $('#search-box').val();
         Session.set('searchQuery', text);
     }, 200)
@@ -37,8 +87,8 @@ Template.website_item.events({
     "click .js-upvote": function(event) {
         // example of how you can access the id for the website in the database
         // (this is the data context for the template)
-        var website_id = this._id;
-        //console.log("Up voting website with id " + website_id);
+        var website_id = this._id,
+            _this = this;
 
         Websites.update({
                 _id: website_id
@@ -47,7 +97,12 @@ Template.website_item.events({
                     upVotes: 1
                 }
             },
-            function(err, res) {});
+            function(err, res) {
+                Session.set('lastTitle', _this.title);
+                $('#suggested_sites').modal('show');
+            });
+
+
 
         return false; // prevent the button from reloading the page
     },
@@ -75,8 +130,8 @@ Template.website.events({
     "click .js-upvote": function(event) {
         // example of how you can access the id for the website in the database
         // (this is the data context for the template)
-        var website_id = this._id;
-        //console.log("Up voting website with id " + website_id);
+        var website_id = this._id,
+            _this = this;
 
         Websites.update({
                 _id: website_id
@@ -85,7 +140,12 @@ Template.website.events({
                     upVotes: 1
                 }
             },
-            function(err, res) {});
+            function(err, res) {
+                Session.set('lastTitle', _this.title);
+                $('#suggested_sites').modal('show');
+            });
+
+
 
         return false; // prevent the button from reloading the page
     },
@@ -109,9 +169,12 @@ Template.website.events({
     },
     'submit .js-add-comment': function(event) {
 
+        var _this = this;
+
         var userEmail = Meteor.users.findOne({
             _id: Meteor.user()._id
         }).emails[0].address;
+
         var comment = {
             content: event.target.comment.value,
             author: {
@@ -130,7 +193,10 @@ Template.website.events({
             },
             function(err, res) {
                 event.target.comment.value = '';
+                Session.set('lastTitle', _this.title);
+                $('#suggested_sites').modal('show');
             });
+
 
         return false;
     }
@@ -152,21 +218,21 @@ Template.website_form.events({
             //...options...
         }, function(err, res) {
 
-           if(!err) {
-            
-            title = $(res.content).filter('title').text();
-            if (title) {
-                $('#title').val(title);
-                Session.set('titleVal', title);
-            }
+            if (!err) {
 
-            description = $(res.content).filter('meta[name="description"]').attr('content');
-            if (description) {
-                $('#description').val(description);
-                Session.set('descriptionVal', description);
-            }
+                title = $(res.content).filter('title').text();
+                if (title) {
+                    $('#title').val(title);
+                    Session.set('titleVal', title);
+                }
 
-           }
+                description = $(res.content).filter('meta[name="description"]').attr('content');
+                if (description) {
+                    $('#description').val(description);
+                    Session.set('descriptionVal', description);
+                }
+
+            }
         });
 
     },
@@ -203,12 +269,3 @@ Template.website_form.helpers({
         return !(Session.get('urlVal') && Session.get('descriptionVal'));
     }
 });
-
-/// Search functionality
-
-// Tracker.autorun(function () {
-//   var cursor = WebsitesIndex.search('course'); // search all docs that contain "Marie" in the name or score field
-
-//   console.log(cursor.fetch()); // log found documents with default search limit
-//   console.log(cursor.count()); // log count of all found documents
-// });
